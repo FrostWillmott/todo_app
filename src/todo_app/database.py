92b07c6e008f136b567840dcd4c5728582f7aae8
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2025-04-16 18:25:33 +0300

    first commit

--- src/todo_app/database.py
+++ src/todo_app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:pyfhew-qygcAh-tigga9@127.0.0.1:3306/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:pyfhew-qygcAh-tigga9@127.0.0.1:3306/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
